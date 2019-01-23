78b66360687f48a727c242ec7138ab29eee90a7a
julius75 <juliusnchagwa75@gmail.com>
2019-01-23 10:57:18 +0300

    first

--- maps.py
+++ maps.py
@@ -10,13 +10,25 @@ latitude = list(data["LAT"])
 longitude = list(data["LON"])
 elevation = list(data["ELEV"])
 
+# function for evevation
+
+
+def color_elevation(elevations):
+    if elevations > 1000:
+        return 'green'
+    elif 1000 <= elevations > 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="MapBox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
 
 # add multitle markers
 for lt, lg, el in zip(latitude, longitude, elevation):
-    fg.add_child(folium.Marker(location=[lt, lg], popup=str(el) + "m", icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt, lg], popup=str(el) + " m", icon=folium.Icon(color=color_elevation(el))))
 
 map.add_child(fg)
 
